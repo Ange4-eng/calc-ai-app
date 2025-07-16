4aa9096187189d9fb1cb992773eb79bc76e16ad7
Tiogang Ladouce <ladoucetiogang@gmail.com>
2025-07-16 13:55:12 +0000

    Correction de l'erreur Vercel 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { MainLayout } from "@/components/main-layout";
 
+// Ce commentaire est ajouté pour forcer un redéploiement sur Vercel.
 export default function Home() {
   return (
     <MainLayout />
